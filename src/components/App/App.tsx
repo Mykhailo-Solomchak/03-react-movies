89b6d9503b3c0f74b67f9a55cd8f662bd11ea752
Mykhailo Solomchak <solomchak8@gmail.com>
2026-01-25 11:02:41 +0000

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import SearchBar from "../SearchBar/SearchBar"
+import css from "./App.module.css"
+import type { Movie } from "../../types/movie"
+import { fetchMovies } from "../../services/movieService"
+import toast from "react-hot-toast"
+import Loader from "../Loader/Loader"
+import ErrorMessage from "../ErrorMessage/ErrorMessage"
+
+function App() {
+    const [movies, setMovies] = useState<Movie[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [isError, setIsError] = useState<boolean>(false)
+    const handleSearch = async (query: string) => {
+        try {
+            setIsError(false)
+            setIsLoading(true)
+            setMovies([])
+            const fetchedMovies = await fetchMovies(query)
+            if (fetchedMovies.length === 0) {
+                toast.error("No movies found for your request.")
+                return
+            }
+            setMovies(fetchedMovies)
+        } catch {
+            setIsError(true)
+        }
+        finally {
+            setIsLoading(false)
+        }
+    }
+    
+    
+    return (
+        <div className={css.app}>
+            <SearchBar onSubmit={handleSearch} />
+            {isLoading&&<Loader/>}
+            {isError&&<ErrorMessage/>}
+        </div>
+    )
+}
+
+export default App
